1b1ecc829dc3e8967df4ea134be38f2ba944fcf8
ulise28 <ulise28@gmail.com>
2020-04-24 21:43:32 +0200

    Logout and profile views

--- src/Auth/Auth.js
+++ src/Auth/Auth.js
@@ -3,6 +3,7 @@ import auth0 from "auth0-js";
 export default class Auth {
   constructor(history) {
     this.history = history;
+    this.userProfile = null;
     this.auth0 = new auth0.WebAuth({
       domain: process.env.REACT_APP_AUTH0_DOMAIN,
       clientID: process.env.REACT_APP_AUTH0_CLIENT_ID,
@@ -44,5 +45,34 @@ export default class Auth {
   isAuthenticated = () => {
     const expiresAt = JSON.parse(localStorage.getItem("expires_at"));
     return new Date().getTime() < expiresAt;
+  };
+
+  logout = () => {
+    localStorage.removeItem("access_token");
+    localStorage.removeItem("id_token");
+    localStorage.removeItem("expires_at");
+    this.userProfile = null;
+    this.auth0.logout({
+      clientID: process.env.REACT_APP_AUTH0_CLIENT_ID,
+      returnTo: "http://localhost:3000",
+    });
+  };
+
+  getAccessToken = () => {
+    const access_token = localStorage.getItem("access_token");
+    if (!access_token) {
+      throw new Error("Access token not found");
+    }
+
+    return access_token;
+  };
+
+  getProfile = (cb) => {
+    if (this.userProfile) return cb(this.userProfile);
+
+    this.auth0.client.userInfo(this.getAccessToken(), (err, profile) => {
+      if (profile) this.userProfile = profile;
+      cb(profile, err);
+    });
   };
 }
